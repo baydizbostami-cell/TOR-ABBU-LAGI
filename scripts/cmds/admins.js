e79da3cf40989baa1e4e2af2fa55a62b19120419
labibvai203 <jannatakterantora@gmail.com>
2026-01-29 12:35:06 +0600

    Update admins.js

--- scripts/cmds/admins.js
+++ scripts/cmds/admins.js
@@ -17,13 +17,13 @@ module.exports = {
         en: {
             listAdmin: "🎭 ADMIN LIST 🎭"
                 + "\n ♦___________________♦"
-                + "\n ❃ OWNER:♣ Ew'r ShAn's ♣"
+                + "\n ❃ OWNER:♣ ꫝhmed Labib ♣"
                 + "\n _____________________________"
                 + "\n _____♪ ADMIN ♪_____"
                 + "\n %1"
                 + "\n _____________________________"
-                + "\n ❃ ♦OWNER♦:https://www.facebook.com/sirana252"
-                + "\n |__S_H_A_N__B_O_T__|",
+                + "\n ❃ ♦OWNER♦:https://www.facebook.com/labib203"
+                + "\n |L__A_B_I_B__B_O_T__|",
             noAdmins: "⚠️ | No admins found in the bot!"
         }
     },
