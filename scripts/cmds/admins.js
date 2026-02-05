d74448777f02631840555fa044b55df87980b0e5
labibvai203 <jannatakterantora@gmail.com>
2026-02-05 12:11:25 +0600

    Update admins.js

--- scripts/cmds/admins.js
+++ scripts/cmds/admins.js
@@ -17,7 +17,7 @@ module.exports = {
         en: {
             listAdmin: "🎭 ADMIN LIST 🎭"
                 + "\n ♦___________________♦"
-                + "\n ❃ OWNER:♣ꫝhmed LꫝBiB♣SuMoN♣MaHfuJ♣"
+                + "\n ❃ OWNER:♣ ꫝhmed LꫝBiB ♣  SuMoN  ♣  MaHfuJ  ♣"
                 + "\n _____________________________"
                 + "\n _____♪ ADMIN ♪_____"
                 + "\n %1"
